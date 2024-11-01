662161f401649b6ed16b3a8266f9237605a325c9
Raimond Kollman <raimond@kollman.nl>
2024-11-01 19:26:15 +0100

    First releases of the import and export history and trends SQL scripts added

---
+++ Migration-PostgreSQL-and-TimescaleDB/export-hist-trends.sql
@@ -0,0 +1,21 @@
+\set ON_ERROR_STOP on
+
+\copy (select * from history_log) TO 'history_log.csv' DELIMITER ',' CSV;
+
+\copy (select * from history) TO 'history.csv' DELIMITER ',' CSV;
+
+\copy (select * from history_str) TO 'history_str.csv' DELIMITER ',' CSV;
+
+\copy (select * from history_text) TO 'history_text.csv' DELIMITER ',' CSV;
+
+\copy (select * from history_log) TO 'history_log.csv' DELIMITER ',' CSV;
+
+
+\copy (select * from history_uint) TO 'history_uint.csv' DELIMITER ',' CSV;
+
+\copy (select * from trends) TO 'trends.csv' DELIMITER ',' CSV;
+
+\copy (select * from trends_uint) TO 'trends_uint.csv' DELIMITER ',' CSV;
+
+\copy (select * from history_bin) TO 'history_bin.csv' DELIMITER ',' CSV;
+
---
+++ Migration-PostgreSQL-and-TimescaleDB/import-hist-trends.sql
@@ -0,0 +1,18 @@
+\set ON_ERROR_STOP on
+
+\copy history FROM 'history.csv' DELIMITER ',' CSV;
+
+\copy history_uint FROM 'history_uint.csv' DELIMITER ',' CSV;
+
+\copy history_log FROM 'history_log.csv' DELIMITER ',' CSV;
+
+\copy history_text FROM 'history_text.csv' DELIMITER ',' CSV;
+
+\copy history_str FROM 'history_str.csv' DELIMITER ',' CSV;
+
+\copy trends FROM 'trends.csv' DELIMITER ',' CSV;
+
+\copy trends_uint FROM 'trends_uint.csv' DELIMITER ',' CSV;
+
+\copy history_bin FROM 'history_bin.csv' DELIMITER ',' CSV;
+
