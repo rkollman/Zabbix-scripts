2f42c1bf933c75a1ecd18e9a4ad2a3e429efc76c
Raimond Kollman <raimond@kollman.nl>
2025-02-10 12:11:02 +0100

    Update export-hist-trends.sql
    Export history tables sorted and double history_log export removed

--- Migration-PostgreSQL-and-TimescaleDB/export-hist-trends.sql
+++ Migration-PostgreSQL-and-TimescaleDB/export-hist-trends.sql
@@ -8,14 +8,10 @@
 
 \copy (select * from history_text) TO 'history_text.csv' DELIMITER ',' CSV;
 
-\copy (select * from history_log) TO 'history_log.csv' DELIMITER ',' CSV;
-
+\copy (select * from history_bin) TO 'history_bin.csv' DELIMITER ',' CSV;
 
 \copy (select * from history_uint) TO 'history_uint.csv' DELIMITER ',' CSV;
 
 \copy (select * from trends) TO 'trends.csv' DELIMITER ',' CSV;
 
 \copy (select * from trends_uint) TO 'trends_uint.csv' DELIMITER ',' CSV;
-
-\copy (select * from history_bin) TO 'history_bin.csv' DELIMITER ',' CSV;
-
