b80977c3874fcb05e6675c4ed3810b205d438abb
Raimond Kollman <raimond@kollman.nl>
2025-02-12 21:08:24 +0100

    Changed CSV to CSV HEADER in SQL-scripts

--- Migration-PostgreSQL-and-TimescaleDB/export-hist-trends.sql
+++ Migration-PostgreSQL-and-TimescaleDB/export-hist-trends.sql
@@ -1,17 +1,17 @@
 \set ON_ERROR_STOP off
 
-\copy (select * from history_log) TO 'history_log.csv' DELIMITER ',' CSV;
+\copy (select * from history_log) TO 'history_log.csv' DELIMITER ',' CSV HEADER;
 
-\copy (select * from history) TO 'history.csv' DELIMITER ',' CSV;
+\copy (select * from history) TO 'history.csv' DELIMITER ',' CSV HEADER;
 
-\copy (select * from history_str) TO 'history_str.csv' DELIMITER ',' CSV;
+\copy (select * from history_str) TO 'history_str.csv' DELIMITER ',' CSV HEADER;
 
-\copy (select * from history_text) TO 'history_text.csv' DELIMITER ',' CSV;
+\copy (select * from history_text) TO 'history_text.csv' DELIMITER ',' CSV HEADER;
 
-\copy (select * from history_bin) TO 'history_bin.csv' DELIMITER ',' CSV;
+\copy (select * from history_bin) TO 'history_bin.csv' DELIMITER ',' CSV HEADER;
 
-\copy (select * from history_uint) TO 'history_uint.csv' DELIMITER ',' CSV;
+\copy (select * from history_uint) TO 'history_uint.csv' DELIMITER ',' CSV HEADER;
 
-\copy (select * from trends) TO 'trends.csv' DELIMITER ',' CSV;
+\copy (select * from trends) TO 'trends.csv' DELIMITER ',' CSV HEADER;
 
-\copy (select * from trends_uint) TO 'trends_uint.csv' DELIMITER ',' CSV;
+\copy (select * from trends_uint) TO 'trends_uint.csv' DELIMITER ',' CSV HEADER;
